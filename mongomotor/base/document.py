937c88d91a2c5beb36eb1dd46435685c0a39e067
Juca Crispim <juca@poraodojuca.net>
2016-07-10 03:32:26 -0300

    correcting dereferencing

--- mongomotor/base/document.py
+++ mongomotor/base/document.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from mongoengine.base import document
+from mongoengine.fields import ReferenceField
+
+
+class BaseDocumentMotor(document.BaseDocument):
+
+    def __init__(self, *args, **kwargs):
+        for name, field in self._fields.items():
+            if isinstance(field, ReferenceField):
+                kwargs['__only_fields'].append(name)
+        super().__init__(*args, **kwargs)

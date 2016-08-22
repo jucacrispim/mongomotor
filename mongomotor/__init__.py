407dd852c49ebe7c1591caa6507f0e6b947f1f6a
Juca Crispim <juca@poraodojuca.net>
2016-08-22 04:05:06 -0300

    version number 0.9b5

--- mongomotor/__init__.py
+++ mongomotor/__init__.py
@@ -5,7 +5,7 @@ from mongoengine.document import MapReduceDocument, EmbeddedDocument
 from mongomotor.document import (Document,
                                  DynamicDocument)
 
-VERSION = '0.9b4'
+VERSION = '0.9b5'
 
 __all__ = ['connect', 'disconnect', 'Document', 'DynamicDocument',
            'EmbeddedDocument', 'MapReduceDocument']

cddf89e2eab3b29d60520c8d8dd9dbde9a914dad
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 04:10:12 -0300

    Gen docs on ci

--- build-scripts/build_docs.sh
+++ build-scripts/build_docs.sh
@@ -8,9 +8,9 @@ echo $PATH
 base_cmd=""
 if [ "$ENV" == "ci" ]
 then
-    base_cmd=".././python-venv/venv-python3.11/bin/"
+    base_cmd="../.././python-venv/venv-python3.11/bin/"
 fi
-ls .././python-venv/venv-python3.11/bin
+ls ../.././python-venv/venv-python3.11/bin
 ls /home/toxicuser/.venv/bin/pip
 "$base_cmd"sphinx-apidoc -o "$PWD/source/apidoc" "$PWD/../mongomotor/"
 "$base_cmd"sphinx-build -b html -d build/doctrees   source build/html/

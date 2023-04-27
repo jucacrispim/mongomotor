9fd5ca8d025d40e742663c646d23a1b2b6f1604b
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 04:25:19 -0300

    Fix upload docs on ci

--- build-scripts/upload_docs.sh
+++ build-scripts/upload_docs.sh
@@ -2,6 +2,6 @@
 
 cd docs/build
 mv html mongomotor
-tar -czf mm.tar.gz mongomotor
+tar -czf mmdocs.tar.gz mongomotor
 
 curl --user "$TUPI_USER:$TUPI_PASSWD" -F 'file=@mmdocs.tar.gz' https://docs.poraodojuca.dev/e/

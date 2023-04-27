b3279b7e93a70670a3b784cca59af4e2f47a88d0
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 02:01:10 -0300

    Debug docs gen on ci

--- build-scripts/build_docs.sh
+++ build-scripts/build_docs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd docs
 export PYTHONPATH="$PWD/../"
+echo "######"
+echo $PATH
+echo "######"
 rm -rf "$PWD/source/apidoc"
 rm -rf "$PWD/build"
-sphinx-apidoc -o "$PWD/source/apidoc" "$PWD/../mongoengine/"
+sphinx-apidoc -o "$PWD/source/apidoc" "$PWD/../mongomotor/"
 make html
-cd -

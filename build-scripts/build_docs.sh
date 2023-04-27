aa66bf332749f60756a0f98d079734c8ffa1095b
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 02:11:36 -0300

    debug gen docs on ci

--- build-scripts/build_docs.sh
+++ build-scripts/build_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd docs
 export PYTHONPATH="$PWD/../"
-export PATH=././python-venv/venv-python3.11/bin:/home/toxicuser/.venv/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=.././python-venv/venv-python3.11/bin:/home/toxicuser/.venv/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin
 echo "######"
 echo $PATH
 echo "######"

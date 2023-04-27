e08d17cb699f889de0675e4df57a9258af457015
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 05:32:02 -0300

    build docs

--- build-scripts/build_docs.sh
+++ build-scripts/build_docs.sh
@@ -3,5 +3,6 @@ cd docs
 export PYTHONPATH="$PWD/../"
 rm -rf "$PWD/source/apidoc"
 rm -rf "$PWD/build"
+echo $PATH
 sphinx-apidoc -o "$PWD/source/apidoc" "$PWD/../mongomotor/"
 sphinx-build -b html -d build/doctrees   source build/html/

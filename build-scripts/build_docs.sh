1aeca5a33f2a30b5dce9357cb19ad9355c750b94
Juca Crispim <jucacrispim@gmail.com>
2023-04-27 02:43:18 -0300

    Debug gen docs on ci
    ci: include-builders Docs

--- build-scripts/build_docs.sh
+++ build-scripts/build_docs.sh
@@ -10,5 +10,7 @@ if [ "$ENV" == "ci" ]
 then
     base_cmd=".././python-venv/venv-python3.11/bin/"
 fi
+ls .././python-venv/venv-python3.11/bin
+ls /home/toxicuser/.venv/bin/pip
 "$base_cmd"sphinx-apidoc -o "$PWD/source/apidoc" "$PWD/../mongomotor/"
 "$base_cmd"sphinx-build -b html -d build/doctrees   source build/html/

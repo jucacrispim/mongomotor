394d838b7eed163640152f7190ffa2b500923194
Juca Crispim <jucacrispim@gmail.com>
2024-05-22 13:19:25 -0300

    Fix docs upload
    ci: include-builders Docs

--- build-scripts/upload_docs.sh
+++ build-scripts/upload_docs.sh
@@ -4,4 +4,4 @@ cd docs/build
 mv html mongomotor
 tar -czf mmdocs.tar.gz mongomotor
 
-curl -F 'file=@mmdocs.tar.gz' https://docs.poraodojuca.dev/e/ -H 'Authorization: Key $TUPI_AUTH_KEY'
+curl -F 'file=@mmdocs.tar.gz' https://docs.poraodojuca.dev/e/ -H "Authorization: Key $TUPI_AUTH_KEY"

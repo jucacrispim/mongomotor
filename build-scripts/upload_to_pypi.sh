06112318763be9277d524e1fa4a837eceb288cb0
Juca Crispim <juca@poraodojuca.dev>
2025-09-29 21:47:48 -0300

    typo

--- build-scripts/upload_to_pypi.sh
+++ build-scripts/upload_to_pypi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[pypi]\nusername = $PYPI_USERNAME\npassword=$PYPI_PASSWORD" > ~/.pypirc
+echo "[pypi]\nusername = $PYPI_USERNAME\npassword = $PYPI_PASSWORD" > ~/.pypirc
 cd dist
 fname=`ls | grep tar`
 twine upload $fname

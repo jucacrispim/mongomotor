28757853ff1f7de19f3f4a0a37b4d81756714639
Juca Crispim <juca@poraodojuca.dev>
2025-09-29 23:10:43 -0300

    Debug ci

--- build-scripts/upload_to_pypi.sh
+++ build-scripts/upload_to_pypi.sh
@@ -2,6 +2,8 @@
 
 confpath=~/.pypirc
 echo "[pypi]\nusername = $PYPI_USERNAME\npassword = $PYPI_PASSWORD" > $confpath
+cat $confpath
+cat pyproject.toml
 cd dist
 fname=`ls | grep tar`
 twine upload -r pypi $fname
